0510bc21949bf37b0068d241d252f8b52db8c9b7
takumiii0625 <132809007+takumiii0625@users.noreply.github.com>
2023-05-27 20:44:29 +0900

    breezeインストール

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,44 +1,59 @@
-@extends('layout')
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col col-md-offset-3 col-md-6">
-            <nav class="panel panel-default">
-                <div class="panel-heading">会員登録</div>
-                <div class="panel-body">
-                    @if($errors->any())
-                    <div class="alert alert-danger">
-                        @foreach($errors->all() as $message)
-                        <p>{{ $message }}</p>
-                        @endforeach
-                    </div>
-                    @endif
-                    <form action="{{ route('register') }}" method="POST">
-                        @csrf
-                        <div class="form-group">
-                            <label for="email">メールアドレス</label>
-                            <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" />
-                        </div>
-                        <div class="form-group">
-                            <label for="name">ユーザー名</label>
-                            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" />
-                        </div>
-                        <div class="form-group">
-                            <label for="password">パスワード</label>
-                            <input type="password" class="form-control" id="password" name="password">
-                        </div>
-                        <div class="form-group">
-                            <label for="password-confirm">パスワード（確認）</label>
-                            <input type="password" class="form-control" id="password-confirm" name="password_confirmation">
-                        </div>
-                        <div class="text-right">
-                            <button type="submit" class="btn btn-primary">送信</button>
-                        </div>
-                    </form>
-                </div>
-            </nav>
-        </div>
-    </div>
-</div>
-@endsection
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+
+            <!-- Name -->
+            <div>
+                <x-label for="name" :value="__('Name')" />
+
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+            </div>
+
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" required />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-button class="ml-4">
+                    {{ __('Register') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\FolderController;
-use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,17 +13,12 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
-*/
 
-Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
-Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
-Route::post('/folders/create', [FolderController::class, 'create']);
-Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
-Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
