946963d45fd08e22f2deffb5aab27c4744e7e6ca
takumiii0625 <132809007+takumiii0625@users.noreply.github.com>
2023-05-27 15:46:47 +0900

    7完完

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Folder;
 use Illuminate\Http\Request;
 use App\Models\Task; // ★ 追加
-use App\Http\Requests\CreateTask;
 
+use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
 
 
 
@@ -50,6 +51,36 @@ class TaskController extends Controller
 
         return redirect()->route('tasks.index', [
             'id' => $current_folder->id,
+        ]);
+    }
+
+    /**
+     * GET /folders/{id}/tasks/{task_id}/edit
+     */
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
+
+
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+        // 1
+        $task = Task::find($task_id);
+
+        // 2
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        // 3
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
         ]);
     }
 }
